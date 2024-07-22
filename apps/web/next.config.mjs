5ae5b5db3519f2c346b14e1c0fe799fb4618c8eb
vishal <vishal@vishals-Air.lan>
2024-07-22 16:39:09 +0530

    added env to next config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript:{
-        ignoreBuildErrors:true
+    typescript: {
+        ignoreBuildErrors: true
     },
-    eslint:{
-        ignoreDuringBuilds:true,
-        ignoreBuildErrors:true
+    eslint: {
+        ignoreBuildErrors: true
+    },
+    env: {
+        MOUNT_PATH: process.env.MOUNT_PATH,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        JWT_SECRET: process.env.JWT_SECRET,
+        DATABASE_URL: process.env.DATABASE_URL,
+        REDIS_URL: process.env.REDIS_URL,
+        JUDGE0_URI: process.env.JUDGE0_URI
     }
 };
 
