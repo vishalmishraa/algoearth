f6f440ffc3e5dc884d1014464ac3e4dd09fa9668
vishal <vishal.mishra9470@gmail.com>
2024-07-27 13:47:09 +0530

    Ad

--- apps/problems/two-sum/boilerplate-full/function.js
+++
@@ -1,8 +0,0 @@
-##USER_CODE_HERE##
-
-const input = require('fs').readFileSync('/dev/problems/two-sum/tests/inputs/##INPUT_FILE_INDEX##.txt', 'utf8').trim().split('\n').join(' ').split(' ');
-const num1 = parseInt(input.shift());
-  const num2 = parseInt(input.shift());
-const result = sum(num1, num2);
-console.log(result);
-    
