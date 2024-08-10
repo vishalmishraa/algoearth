a41e90f59acd934121581ee36594299caab98695
vishalmishraa <vishal.mishra9470@gmail.com>
2024-08-10 15:03:33 +0530

    working

--- packages/db/prisma/updateQuestion.ts
+++ packages/db/prisma/updateQuestion.ts
@@ -5,70 +5,93 @@ import prismaClient from "../src/index";
 const MOUNT_PATH = process.env.MOUNT_PATH ?? "../../../apps/problems";
 
 function promisifedReadFile(path: string): Promise<string> {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, "utf8", (err, data) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(data);
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, "utf8", (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(data);
+        });
     });
-  });
 }
 
 async function main(problemSlug: string, problemTitle: string) {
-  const problemStatement = await promisifedReadFile(
-    `${MOUNT_PATH}/${problemSlug}/Problem.md`
-  );
+    const problemStatement = await promisifedReadFile(
+        `${MOUNT_PATH}/${problemSlug}/Problem.md`
+    );
+    const tags = await promisifedReadFile(
+        `${MOUNT_PATH}/${problemSlug}/tags.md`
+    );
 
-  const problem = await prismaClient.problem.upsert({
-    where: {
-      slug: problemSlug,
-    },
-    create: {
-      title: problemSlug,
-      slug: problemSlug,
-      description: problemStatement,
-      hidden: false,
-    },
-    update: {
-      description: problemStatement,
-    },
-  });
+    const tagsArray = tags.split("\n").map(tag => tag.trim());
 
-  await Promise.all(
-    Object.keys(LANGUAGE_MAPPING).map(async (language) => {
-      const code = await promisifedReadFile(
-        `${MOUNT_PATH}/${problemSlug}/boilerplate/function.${language}`
-      );
+    const connectOrCreateTags = tagsArray.map(tag => ({
+        where: { name: tag },
+        create: { name: tag }
+    }));
 
-      await prismaClient.defaultCode.upsert({
+    console.log(connectOrCreateTags);
+
+    const problem = await prismaClient.problem.upsert({
         where: {
-          problemId_languageId: {
-            problemId: problem.id,
-            languageId: LANGUAGE_MAPPING[language].internal,
-          },
+            slug: problemSlug,
         },
         create: {
-          problemId: problem.id,
-          languageId: LANGUAGE_MAPPING[language].internal,
-          code,
+            title: problemSlug,
+            slug: problemSlug,
+            description: problemStatement,
+            hidden: false,
+            tags: {
+                connectOrCreate: tagsArray.map(tag => ({
+                    where: { name: tag },
+                    create: { name: tag }
+                })),
+            }
         },
         update: {
-          code,
+            description: problemStatement,
+            tags: {
+                connect: tagsArray.map(tag => ({
+                    where: { name: tag }
+                }))
+            } 
         },
-      });
-    })
-  );
+    });
+
+    await Promise.all(
+        Object.keys(LANGUAGE_MAPPING).map(async (language) => {
+            const code = await promisifedReadFile(
+                `${MOUNT_PATH}/${problemSlug}/boilerplate/function.${language}`
+            );
+
+            await prismaClient.defaultCode.upsert({
+                where: {
+                    problemId_languageId: {
+                        problemId: problem.id,
+                        languageId: LANGUAGE_MAPPING[language].internal,
+                    },
+                },
+                create: {
+                    problemId: problem.id,
+                    languageId: LANGUAGE_MAPPING[language].internal,
+                    code,
+                },
+                update: {
+                    code,
+                },
+            });
+        })
+    );
 }
 
 export function addProblemsInDB() {
-  fs.readdir(MOUNT_PATH, (err, dirs) => {
-    if (err) {
-      console.error("Error reading directory:", err);
-      return;
-    }
-    dirs.forEach(async (dir) => {
-      await main(dir, dir);
+    fs.readdir(MOUNT_PATH, (err, dirs) => {
+        if (err) {
+            console.error("Error reading directory:", err);
+            return;
+        }
+        dirs.forEach(async (dir) => {
+            await main(dir, dir);
+        });
     });
-  });
 }
