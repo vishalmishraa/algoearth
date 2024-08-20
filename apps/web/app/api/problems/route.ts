3a6cb665830ea07847800bd0ccaa6cbe8d8f7404
vishalmishraa <vishal.mishra9470@gmail.com>
2024-08-20 21:28:36 +0530

    added api/problems

--- apps/web/app/api/problems/route.ts
+++ apps/web/app/api/problems/route.ts
@@ -1,17 +1,44 @@
 import db from "@/db";
-import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../../../lib/auth";
 
-export async function GET(req: Request) {
-    const problems = await db.problem.findMany({
-        where: {
-            contests: {
-                some: {}
+
+export async function GET(req: NextRequest) {
+    //Check if user is logged in
+    try {
+        const session = await getServerSession(authOptions);
+        if (!session?.user) {
+            return NextResponse.json(
+                {
+                    message: "You must be logged in to submit a problem",
+                    status: 401
+                }
+            );
+        };
+
+        //Check if user is allowed to submit
+        //get contest by user id 
+        const problems = await db.problem.findMany({
+            include: {
+                contests: {
+                    select: {
+                        contestId: true
+                    }
+                }
             }
-        },
-        include: {
-            contests: true
-        }
-    });
+        });
 
-    return NextResponse.json({ problems });
-}
+
+        return NextResponse.json(problems);
+    } catch (error) {
+        console.error("Error fetching problems:", error);
+        return NextResponse.json(
+            {
+                message: "An error occurred while fetching problems",
+                status: 500
+            }
+        );
+    }
+
+};
