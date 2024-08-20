813bb8189c792ee76555cf9730721e2caa20251f
vishalmishraa <vishal.mishra9470@gmail.com>
2024-08-20 22:56:18 +0530

    butify hero page button

--- apps/web/components/Hero.tsx
+++ apps/web/components/Hero.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 export function Hero() {
   return (
@@ -15,18 +16,16 @@ export function Hero() {
             </p>
             <div className="flex gap-4 pt-16">
               <Link
-                href="/contests"
-                className="inline-flex items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-white shadow transition-colors hover:bg-gray-900/90 focus:outline-none focus:ring-1 focus:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus:ring-gray-300"
+                href="/contests-dashboard"
                 prefetch={false}
               >
-                View Contests
+                <Button className="px-20 py-6 rounded-full border-2 border-gray-100" variant={'secondary'}>Contests Dashboard</Button>
               </Link>
               <Link
-                href="/contests-dashboard"
-                className="inline-flex items-center justify-center rounded-md border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 shadow-sm transition-colors hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:text-gray-50 dark:hover:bg-gray-800 dark:focus:ring-gray-300"
+                href="/contests"
                 prefetch={false}
               >
-                Create Contests
+                <Button className="px-20 py-6 rounded-full border-gray-300" variant={'outline'}>{"View Contest"}</Button>
               </Link>
             </div>
           </div>
