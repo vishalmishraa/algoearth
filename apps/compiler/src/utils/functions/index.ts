e0caffd37c28c449816bb306189b6e7b28469850
vishalmishraa <vishal.mishra9470@gmail.com>
2024-08-27 19:46:42 +0530

    Added compiler service

---
+++ apps/compiler/src/utils/functions/index.ts
@@ -0,0 +1,30 @@
+export function getCommnad(language: string) {
+
+    let command = '';
+
+    switch (language) {
+        case 'cpp':
+            command = 'g++ ./box/yourcode.cpp  -o ./box/yourcode && ./box/yourcode';
+            break;
+        case 'python':
+            command = 'python3 ./box/yourcode.py';
+            break;
+        case 'c':
+            command = 'gcc -o output ./box/yourcode.c && ./box/output';
+            break;
+        case 'java':
+            command = 'javac ./box/yourcode.java && java ./box/yourcode';
+            break;
+        case 'javascript':
+            command = 'node ./box/yourcode.js';
+            break;
+        default:
+            return {
+                error: `Unsupported language for problem: ${language}`
+            }
+    }
+
+    return {
+        command
+    }
+}
