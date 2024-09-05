ed7afe0c1d1e89cb9da344154c20f90ed7ccbb72
vishalmishraa <vishal.mishra9470@gmail.com>
2024-09-05 12:13:00 +0530

    removed unsupported languages for now

--- packages/common/language/index.ts
+++ packages/common/language/index.ts
@@ -8,9 +8,6 @@ export const LANGUAGE_MAPPING: {
 } = {
   js: { judge0: 63, internal: 1, name: "Javascript", monaco: "javascript" },
   cpp: { judge0: 54, internal: 2, name: "C++", monaco: "cpp" },
-  rs: { judge0: 73, internal: 3, name: "Rust", monaco: "rust" },
-  java: { judge0: 62, internal: 4, name: "Java", monaco: "java" },
-  py: { judge0: 71, internal: 5, name: "Python", monaco: "python" },
 };
 
 
